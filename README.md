85b82462f2cf530cc81527c01a16ed222f63043f
Reena <reenam2017@gmail.com>
2018-06-21 21:40:37 -0700

    new update

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # TriviaGame
 This is a quiz game.
+
+What the project does?
+    This is basically a quiz game. Here the user will be asked question on a particular topic and the user have to give the correct answer. The user have to answer all question in a given time period.
+
+Why the project is useful?
+    The project help the user to gain some knowledge and to refresh their knowledge.
+
+How users can get started with the project?
+    This is a very simple game.
+
+Who maintains and contribute to the project?
+    This project is maintained by Reena Mahapatra.
+
+
+Pseudocode:-
+When the page loads with the click of the button, the countdown timer starts.
+a list of questions with 4 different answer options with radio button available.
+when the user clicks the radio button, if the ans is correct then the correct-ans score increses by 1.
+when the user clicks the radio button, if the ans is wrong then the wrong-ans score increses by 1.
+at the end whem the user has answered all question then a modal window pops of displaying the summarry of the game with the options
+  of playing the game again or exiting.
+
+countdown timer:
+set the timer to 60
+create a run function
+create a stop Function
+create a decrement function where the timer is decremented and the stop function is called when the timer is 0 and a popup message is displayed.
---
+++ assets/javascript/app.js
@@ -0,0 +1,88 @@
+//Initializing the variables....................................
+var timeLeft = 10;
+var intervalId;
+var correctAns = 0;
+var wrongAns = 0;
+var questionContainer = document.getElementById("question");
+
+console.log("hello");
+//This function displayTimer is called when the start button is clicked......................
+function displayTimer() {
+     //Creating the run function which will display the question page with the countdown timer...................
+     console.log("I'm inside displayTimer");
+    function run() {
+        //Displaying the timer and question page............................................
+        document.getElementById("main-page").style.display = 'block';
+        //Hidding the instruction page and start button................................
+        document.getElementById("instruction").style.display = 'none';
+        clearInterval(intervalId);
+        //Displaying the timer's initial value.....................................
+        document.getElementById("timer").innerHTML = "<h2>Time Left: " + 10 + "</h2>";
+        intervalId = setInterval(decrement, 1000);
+        //Calling the listQuestion function.....................................
+        //document.getElementById("question").innerHTML = question;
+        document.getElementById("submit").style.display = 'block';
+        console.log("I'm inside run");
+    }
+    //  The decrement function..................................................
+    function decrement() {
+        //Decrease timwLeft by one.
+        timeLeft--;
+        //Show the timeLeft in the #timer tag.
+        document.getElementById("timer").innerHTML = "<h2>Time Left: " + timeLeft + "</h2>";
+        //Once number hits zero...
+        if (timeLeft === 0) {
+            //Run the stop function.
+            stop();
+            finalScore();
+            console.log("I'm inside decrement");
+        }
+    }
+
+     //The stop function
+     function stop() {
+         //Clears our intervalId, we just pass the name of the interval to the clearInterval function.
+         clearInterval(intervalId);
+     }
+     //  Execute the run & questionPage function.
+     run();
+}   //dispalyTimer() end bracket................................................
+
+//This function will be called in the modal window when the timer is 0 or the submit button is clicked
+//and it displays the final score of the quiz.............
+function finalScore() {
+  document.getElementById("modal-window").style.display = 'block';
+  document.getElementById("main-page").style.display = 'none';
+  var message = "<h3>Game Over!</h3>" + "<p>Total question:" + 10 + "</p>" +
+                "<p>Total Correct Answers: " + correctAns + "</p>" +
+                "<p>Total Wrong Answers: " + wrongAns + "</p>";
+  document.getElementById("modal2").innerHTML = message;
+}
+//This function is called when the Main Page button in modal window is clicked, which takes you to the instruction page.....................
+function mainpage() {
+  timeLeft = 10;
+  correctAns = 0;
+  wrongAns = 0;
+  document.getElementById("instruction").style.display = 'block';
+  document.getElementById("main-page").style.display = 'none';
+  document.getElementById("modal-window").style.display = 'none';
+  console.log("I'm inside mainpage");
+}
+
+//This function is called when the Replay button in modal window is clicked, which restart the quiz by resetting all the values and the answers.....................
+function replay() {
+  timeLeft = 60;
+  correctAns = 0;
+  wrongAns = 0;
+  document.getElementById("modal-window").style.display = 'none';
+  displayTimer();
+}
+
+function isChecked() {
+  var correctChecked = document.getElementById("correctA").checked;
+  correctAns ++;
+  var incorrectChecked = document.getElementById("incorrectA").checked;
+  wrongAns ++;
+  if ()
+}
+////I HAVE TO CREATE A FOR LOOP BUT DONT KNOW HOW TO DO IT...
